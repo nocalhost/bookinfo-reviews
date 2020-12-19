316b2e7437c52471e881697c856951817e6e768f
王振威 <wangzhenwei@coding.net>
2020-12-19 22:18:08 +0800

    try to fix ratings

--- src/main/java/dev/nocalhost/reviews/ReviewsApplication.java
+++ src/main/java/dev/nocalhost/reviews/ReviewsApplication.java
@@ -1,11 +1,22 @@
 package dev.nocalhost.reviews;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Collections;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.StringReader;
 
 @SpringBootApplication
 @Controller
@@ -18,7 +29,7 @@ public class ReviewsApplication {
 		app.run(args);
 	}
 
-	private final static Boolean ratings_enabled = Boolean.valueOf(System.getenv("ENABLE_RATINGS"));
+	private final static Boolean ratings_enabled = true;
 	private final static String star_color = System.getenv("STAR_COLOR") == null ? "black" : System.getenv("STAR_COLOR");
 	private final static String services_domain = System.getenv("SERVICES_DOMAIN") == null ? "" : ("." + System.getenv("SERVICES_DOMAIN"));
 	private final static String ratings_hostname = System.getenv("RATINGS_HOSTNAME") == null ? "ratings" : System.getenv("RATINGS_HOSTNAME");
@@ -117,12 +128,58 @@ public class ReviewsApplication {
 		return "{\"status\": \"Reviews is healthy\"}";
 	}
 
+    private JsonObject getRatings(String productId, HttpHeaders requestHeaders) {
+		ClientBuilder cb = ClientBuilder.newBuilder();
+		Integer timeout = star_color.equals("black") ? 10000 : 2500;
+		cb.property("com.ibm.ws.jaxrs.client.connection.timeout", timeout);
+		cb.property("com.ibm.ws.jaxrs.client.receive.timeout", timeout);
+		Client client = cb.build();
+		WebTarget ratingsTarget = client.target(ratings_service + "/" + productId);
+		Invocation.Builder builder = ratingsTarget.request(MediaType.APPLICATION_JSON);
+		for (String header : headers_to_propagate) {
+		  String value = requestHeaders.getHeaderString(header);
+		  if (value != null) {
+			builder.header(header,value);
+		  }
+		}
+		try {
+		  Response r = builder.get();
+  
+		  int statusCode = r.getStatusInfo().getStatusCode();
+		  if (statusCode == Response.Status.OK.getStatusCode()) {
+			try (StringReader stringReader = new StringReader(r.readEntity(String.class));
+				 JsonReader jsonReader = Json.createReader(stringReader)) {
+			  return jsonReader.readObject();
+			}
+		  } else {
+			System.out.println("Error: unable to contact " + ratings_service + " got status of " + statusCode);
+			return null;
+		  }
+		} catch (ProcessingException e) {
+		  System.err.println("Error: unable to contact " + ratings_service + " got exception " + e);
+		  return null;
+		}
+	  }
 	@RequestMapping(value = "/reviews/{productId}", method = RequestMethod.GET)
 	@ResponseBody
 	public String bookReviewsById(@PathVariable("productId") int productId) {
 		int starsReviewer1 = -1;
 		int starsReviewer2 = -1;
 
+		if (ratings_enabled) {
+			JsonObject ratingsResponse = getRatings(Integer.toString(productId), requestHeaders);
+			if (ratingsResponse != null) {
+			  if (ratingsResponse.containsKey("ratings")) {
+				JsonObject ratings = ratingsResponse.getJsonObject("ratings");
+				if (ratings.containsKey("Reviewer1")){
+					starsReviewer1 = ratings.getInt("Reviewer1");
+				}
+				if (ratings.containsKey("Reviewer2")){
+				  starsReviewer2 = ratings.getInt("Reviewer2");
+				}
+			  }
+			}
+		  } 
 		return getJsonResponse(Integer.toString(productId), starsReviewer1, starsReviewer2);
 	}
 
